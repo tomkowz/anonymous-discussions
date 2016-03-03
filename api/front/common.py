b59f4fb8b9063ef2d804225a06bc3be5c40ac509
Tomasz Szulc <mail@szulctomasz.com>
2016-03-03 23:04:57 +0100

    Add constraints about text length

--- api/front/common.py
+++ api/front/common.py
@@ -24,13 +24,17 @@ def add():
 def add_post():
     content = flask.request.form['content']
     min_len = 10
-    if len(content) > min_len:
+    max_len = 200
+
+    if len(content) < min_len:
+        error = u'Wpis jest zbyt krótki (min. {} znaków).'.format(min_len)
+    elif len(content) > max_len:
+        error = u'Wpis jest zbyt długi (max. {} znaków).'.format(max_len)
+    else:
         entry = InsertEntryCoordinator.insert_entry({'content': content})
         if entry is not None:
             return flask.redirect(flask.url_for('main'))
         else:
             error = u'Nie udało się dodać wpisu. Spróbuj ponownie.'
-    else:
-        error = u'Wpis jest zbyt krótki (min. {} znaków).'.format(min_len)
-
+            
     return flask.render_template('add.html', title=u'Nowy wpis', content=content, error=error)
