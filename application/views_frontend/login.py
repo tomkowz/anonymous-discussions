8ffbd5f685b07b078c3a2e8413ff8ed25f97b634
Tomasz Szulc <mail@szulctomasz.com>
2016-03-06 19:08:22 +0100

    Cleanup login.py

--- application/views_frontend/login.py
+++ application/views_frontend/login.py
@@ -9,30 +9,26 @@ from application.views_frontend.presentable import PresentableEntry
 
 @app.route('/admin/login', methods=['GET'])
 def show_admin_login(username=None, password=None, error=None):
-    return flask.render_template(
-            'admin/login.html',
-            username=username,
-            password=password,
-            title='Logowanie (Admin)',
-            error=error)
+    return flask.render_template('admin/login.html', username=username, password=password,
+                                 title='Logowanie (Admin)', error=error)
 
 @app.route('/admin/login', methods=['POST'])
 def admin_login():
     username = flask.request.form['username']
     password = flask.request.form['password']
     if Admin.login(username, password) == True:
-        entries = Entry.get_all_waiting_to_aprove()
-        p_entries = [PresentableEntry(e) for e in entries]
+        p_entries = [PresentableEntry(e) for e in Entry.get_all_waiting_to_aprove()]
         return flask.render_template('admin/entries_to_approve.html', title='Moderacja', p_entries=p_entries)
     else:
         return show_admin_login(username, password, 'Niepoprawne dane.')
 
 @app.route('/admin/entries/<id>/approve/<approved>')
 def admin_approve_entry(id, approved):
+    # Update entry
     entry = Entry.get_with_id(id)
     entry.approved = approved
     entry.save()
 
-    entries = Entry.get_all_waiting_to_aprove()
-    p_entries = [PresentableEntry(e) for e in entries]
+    # Refresh
+    p_entries = [PresentableEntry(e) for e in Entry.get_all_waiting_to_aprove()]
     return flask.render_template('admin/entries_to_approve.html', title='Moderacja', p_entries=p_entries)
