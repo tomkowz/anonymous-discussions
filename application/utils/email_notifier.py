58ba337622a26e39821ade12003041e4f3fac3fd
Tomasz Szulc <mail@szulctomasz.com>
2016-03-06 22:52:01 +0100

    Reconfigure email service to use with pythonanywhere

--- application/utils/email_notifier.py
+++ application/utils/email_notifier.py
@@ -8,14 +8,15 @@ class EmailNotifier:
 
     @staticmethod
     def notify_about_new_post():
+        email = 'spowiedzwszafie@gmail.com'
         msg = MIMEText('Nowy wpis oczekuje na moderację.')
         msg['Subject'] = 'spowiedzwszafie.pl - nowy wpis'
-        msg['From'] = 'noreply@spowiedzwszafie.pl'
-        msg['To'] = 'mail+spowiedzwszafie@szulctomasz.com'
+        msg['From'] = email
+        msg['To'] = 'email
 
         try:
             s = smtplib.SMTP('localhost')
-            s.sendemail(msg['From'], [msg['To']], msg.as_string())
+            s.sendemail(email, [email], msg.as_string())
             s.quit()
         except:
             print 'Cannot send email about new post.'
