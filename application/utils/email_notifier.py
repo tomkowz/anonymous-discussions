f91ad83e55915523b851271866f3c263aea6db67
Tomasz Szulc <mail@szulctomasz.com>
2016-03-07 19:06:19 +0100

    Use official spowiedzwszafie.pl email for sending out notifications

--- application/utils/email_notifier.py
+++ application/utils/email_notifier.py
@@ -8,16 +8,17 @@ class EmailNotifier:
 
     @staticmethod
     def notify_about_new_post():
+        notification_email = 'notification@spowiedzwszafie.pl'
         key = 'key-834b6ffa64e1d46bc418369d7c25c88a'
         sandbox = 'sandboxea724f4294444c2fbc6090aa621fdcc7.mailgun.org'
-        recipient = 'spowiedzwszafie@gmail.com'
+        recipient = notification_email
 
         request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(sandbox)
         request = requests.post(request_url, auth=('api', key), data={
-            'from': 'spowiedzwszafie@gmail.com',
-            'to': recipient,
+            'from': notification_email,
+            'to': 'spowiedzwszafie@gmail.com',
             'subject': 'Nowy wpis oczekuje na moderację.',
-            'text': 'Nowy wpis oczekuje na moderację.'
+            'text': 'Nowy wpis oczekuje na moderację - http://spowiedzwszafie.pl/admin/login'
         })
 
         # print 'Status: {0}'.format(request.status_code)
