7896c7358ab742c07233ead1197e3399ad48e502
Tomasz Szulc <mail@szulctomasz.com>
2016-03-23 18:14:24 +0100

    Disable buttons after voting

--- application/static/votebox.js
+++ application/static/votebox.js
@@ -1,11 +1,21 @@
 function votebox_vote(type, id, val) {
+  var key_path = '.' + type + '-' + id + ' .votebox .votebox-';
+  var btn_up = $(key_path + 'up');
+  var btn_down = $(key_path + 'down');
+
   var path = '/_votebox';
   $.getJSON(path, {
     object_type: type,
     object_id: id,
     value: val
   }, function(data) {
-     $('.' + type + '-' + id + ' .votebox .votebox-up').text('+' + data.up);
-     $('.' + type + '-' + id + ' .votebox .votebox-down').text('-' + data.down);
+
+    // Update values
+    btn_up.text('+' + data.up);
+    btn_down.text('-' + data.down);
+
+    // Disable buttons
+    btn_up.attr('onclick', '');
+    btn_down.attr('onclick', '');
   });
 }
