f71060d241ebba637a2fff8038d204e148270ac2
Tomasz Szulc <mail@szulctomasz.com>
2016-03-29 15:07:32 +0200

    Add error logging for token management

--- application/mod_web/views_token.py
+++ application/mod_web/views_token.py
@@ -13,11 +13,8 @@ from application.mod_web.utils_user_notifications import \
 @app.route('/ustawienia/twoj_token', methods=['GET'])
 def show_your_token_settings():
     user_token = flask.request.cookies.get('op_token', None)
-    user_notifications_count = utils_get_user_notifications_count(user_token)
-    return flask.render_template('web/token_management.html',
-        title=u'Twój token',
-        user_token=user_token,
-        user_notifications_count=user_notifications_count)
+    return _render_view(user_token=user_token, error=None)
+
 
 @app.route('/token/generuj', methods=['GET'])
 def generate_token():
@@ -31,8 +28,8 @@ def generate_token():
         response.set_cookie('op_token', user_token)
         return response
     else:
-        return flask.redirect(flask.url_for('show_your_token_settings',
-                user_token=flask.request.cookies.get('op_token', None)))
+        return _render_view(user_token=flask.request.cookies.get('op_token', None),
+                    error="Nie udało się wygenerować nowego tokena. Spróbuj ponownie." )
 
 
 @app.route('/token/zmien', methods=['POST'])
@@ -42,10 +39,22 @@ def change_token():
     if success == 200:
         # Change only to token if it exists (in db), do not allow to pass custom ones
         if json.loads(response_check.data)['exists'] is True:
-            response = app.make_response(flask.redirect(flask.url_for('show_your_token_settings', user_token=user_op_token)))
+            response = app.make_response(flask.redirect(flask.url_for('show_your_token_settings',
+                user_token=user_op_token,
+                error=None)))
             response.set_cookie('op_token', user_op_token)
             return response
-            
+
     # If error during changing token
-    return flask.redirect(flask.url_for('show_your_token_settings',
-            user_token=flask.request.cookies.get('op_token', None)))
+    return  _render_view(user_token=flask.request.cookies.get('op_token', None),
+                error="Nie udało się zmienić tokena. Upewnij się, że jest \
+                    poprawny lub wygeneruj nowy.")
+
+
+def _render_view(user_token=None, error=None):
+    user_notifications_count = utils_get_user_notifications_count(user_token)
+    return flask.render_template('web/token_management.html',
+        title=u'Twój token',
+        user_token=user_token,
+        user_notifications_count=user_notifications_count,
+        error=error)
